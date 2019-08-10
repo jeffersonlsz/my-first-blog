7a41cc379e238ec90b1e560b961f5a4e94b9e6d4
jefferson <jeffersonlsz@gmail.com>
2019-08-10 16:47:40 -0300

    modified template displaying

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
 def index(request):
     return render(request, 'index.html', {})
